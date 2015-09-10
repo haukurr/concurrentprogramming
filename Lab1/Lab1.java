2ceb5707646ed3ed6b0c28c456876a1c10e4bd5a
Haukur Rosinkranz <haukurr@remote11.chalmers.se>
2015-09-10 18:36:10 +0200

    update!

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -4,33 +4,52 @@ import java.util.Arrays;
 
 public class Lab1 {
 
-    private final Semaphore[] semaphores = new Semaphore[1];
+    private final Semaphore[] semaphores = new Semaphore[9];
     private Train top_train;
     private Train bottom_train;
+    private int simulation_speed;
+
+    private enum Direction {
+        UP,DOWN
+    };
 
     private class Train extends Thread {
-        private final int TIME_AT_STATION = 1;
+        private final int TIME_AT_STATION = 1500;
         private final int MAXSPEED = 19;
 
         private int id;
         private int speed;
-        private int sim_speed;
         private TSimInterface tsi;
+        private Direction direction;
 
         private int sensors[][] = {
-            {2,9}
+            {7,11},
+            {9,9}
         };
 
-        private int speedAtStation() {
-            return this.TIME_AT_STATION + 2 * this.sim_speed * Math.abs(this.speed);
+        private void setSwitch(int i, Direction direction) {
+            int switches[][] = {
+                {3,11},
+                {4,9},
+                {15,9},
+                {17,7}
+            };
+            try {
+                this.tsi.setSwitch(switches[i][0], switches[i][1], direction.ordinal()+1);
+            } catch(CommandException e) { System.exit(1); }
         }
 
-        public Train(int id, int train_speed, int simulation_speed) {
+        private int speedAtStation() {
+            return this.TIME_AT_STATION + 2 * simulation_speed * Math.abs(this.speed);
+        }
+
+        public Train(int id, int train_speed, Direction d) {
             super();
             this.id = id;
             this.tsi = TSimInterface.getInstance();
-            this.sim_speed = simulation_speed;
+            this.direction = d;
             this.setSpeed(train_speed);
+            this.setSwitch(1,Direction.DOWN);
         }
 
         public void setSpeed(int speed) {
@@ -54,23 +73,30 @@ public class Lab1 {
                 try {
                     SensorEvent sensor = tsi.getSensor(this.id);
                     if (sensor.getStatus() == 1) {
-                        int pos[] = {sensor.getXpos(), sensor.getYpos()};
+                        int pos[] = { sensor.getXpos(), sensor.getYpos() };
                         for(int i=0;i<sensors.length;i++) {
                             if(Arrays.equals(pos,sensors[i])) {
-
-                                switch(i) {
-                                    case 0:
-                                    System.exit(1);
-                                    break;
+                                if(this.direction == Direction.UP) {
+                                    switch(i) {
+                                        case 0: case 1:
+                                        this.setSpeed(10);
+                                        break;
+                                    }
                                 }
-
+                                if(this.direction == Direction.DOWN) {
+                                    switch(i) {
+                                        case 0: case 1:
+                                        this.setSpeed(10);
+                                        break;
+                                    }
+                                } else
                                 break;
                             }
                         }
                     }
                 }
-                catch(CommandException e) {}
-                catch(InterruptedException e) {}
+                catch(CommandException e)     { System.exit(1); }
+                catch(InterruptedException e) { System.exit(1); }
             }
         }
 
@@ -78,9 +104,9 @@ public class Lab1 {
 
     public Lab1(String[] args) {
         //DEFAULTS
-        int top_train_speed = 0;
-        int bottom_train_speed = 0;
-        int simulation_speed = 100;
+        int simulation_speed = 20;
+        int bottom_train_speed 5;
+        int top_train_speed = bottom_train_speed;
 
         try {
             top_train_speed = Integer.parseInt(args[0]);
@@ -98,8 +124,8 @@ public class Lab1 {
             semaphores[i] = new Semaphore(1,true);
         }
 
-        this.top_train = new Train(1, top_train_speed, simulation_speed);
-        this.bottom_train = new Train(2, bottom_train_speed, simulation_speed);
+        this.top_train = new Train(1, top_train_speed, Direction.DOWN);
+        this.bottom_train = new Train(2, bottom_train_speed, Direction.UP);
 
         this.top_train.start();
         this.bottom_train.start();
