e8edae32b8c517cffcea2fa2450257350e9dade1
Alexander Hederstaf <alexander.hederstaf@gmail.com>
2015-10-04 23:30:39 +0200

    Comments for Definitions

--- Lab3/defs.hrl
+++ Lab3/defs.hrl
@@ -2,10 +2,21 @@
 % Add whatever other fields you need.
 % It contains the following fields:
 %   gui: the name (or Pid) of the GUI process.
+%   nick: the string name of the client.
+%   server: the string name of the server.
+%   channels: a list of tuples with the {name, pid} of the channel.
 -record(client_st, {gui, nick, server, channels}).
 
 % This record defines the structure of the server process.
 % Add whatever other fields you need.
+% It contains the following fields:
+%   name: the string name of the server.
+%   users: a list of tuples with the {pid, name} of the users.
+%   channels: a list of strings with the registered channel names.
 -record(server_st, {name, users, channels}).
 
+% This record defines the structure of the channel process.
+% It contains the following fields:
+%   name: the string name of the channel.
+%   users: a list of tuples with the {pid, name} of the users.
 -record(channel_st, {name,users}).
